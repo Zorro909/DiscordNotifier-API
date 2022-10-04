4d10c545080d986bcf4dbb9b5f4b746f308c8962
Zorro909 <an@maximilian-kling.de>
2022-10-04 20:31:06 +0000

    Initial commit

---
+++ src/main/java/de/zorro909/discordnotifier/api/Application.java
@@ -0,0 +1,9 @@
+package de.zorro909.discordnotifier.api;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/de/zorro909/discordnotifier/api/DiscordNotifierAPITest.java
@@ -0,0 +1,21 @@
+package de.zorro909.discordnotifier.api;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class DiscordNotifierAPITest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
